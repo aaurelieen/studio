aa60261a567619fd7475e374642ec7eba88e817b
aaurelieen <aurelienhadoux@gmail.com>
2022-05-06 17:33:56 +0200

    head

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'studio',
+    title: 'ANWH® | Independent Web Design & Development Studio.',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Independent Web Design & Development Studio. Based in Paris, specializing in Development and Webdesign. We work particularly with qualified freelancers.' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
